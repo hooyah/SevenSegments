fdc9302dff63965f1672b7396723970d79867f39
Florian <florian.pubhooyah@gmail.com>
2020-07-11 13:21:11 +1200

    switched back to http/1.0 to avoid dealing with chunked replies added error (double)blink code

--- getSubCount_ESP01_arduino/getSubCount.ino
+++ getSubCount_ESP01_arduino/getSubCount.ino
@@ -22,6 +22,7 @@ const char* host = "www.googleapis.com";
 String url = "/youtube/v3/channels?part=statistics&id=<YOUR_CHANNEL_ID>&fields=items/statistics/subscriberCount&key=<YOUR_API_KEY>";
 const int httpsPort = 443;
 
+int everythingIsFine = true;
 // Use web browser to view and copy SHA1 fingerprint of the certificate
 // Forget about the finger print. You'll have to update it every time the certificate changes
 //const char fingerprint[] PROGMEM = "43 2f 75 94 fb 9c 00 ab ee 26 22 61 57 50 bf b4 d2 05 85 c7";
@@ -41,23 +42,27 @@ int get_subscriber_count()
 
   if (!client.connect(host, httpsPort)) {
     Serial.println("connection failed");
+    everythingIsFine = false;
     return -1;
   }
 
-  if(client.print(String("GET ") + url + " HTTP/1.1\r\n" +
-               "Host: " + host + "\r\n" +
-               "Connection: close\r\n\r\n") == 0)
+  // Yay, googleapis suddenly replied with chunks, so switching back to 1.0
+  if (client.print(String("GET ") + url + " HTTP/1.0\r\n" +
+                   "Host: " + host + "\r\n" +
+                   "Connection: close\r\n\r\n") == 0)
   {
     Serial.println("Failed to send http request");
-    return -1; 
+    everythingIsFine = false;
+    return -1;
   }
 
   // Check HTTP status
   char status[32] = {0};
   client.readBytesUntil('\r', status, sizeof(status));
-  if (strcmp(status, "HTTP/1.1 200 OK") != 0) {
+  if (strcmp(status, "HTTP/1.0 200 OK") != 0) {
     Serial.print("Unexpected response: ");
     Serial.println(status);
+    everythingIsFine = false;
     return -1;
   }
 
@@ -65,10 +70,19 @@ int get_subscriber_count()
   char endOfHeaders[] = "\r\n\r\n";
   if (!client.find(endOfHeaders)) {
     Serial.println("Failed to read header");
+    everythingIsFine = false;
     return -1;
   }
 
   const size_t capacity = JSON_OBJECT_SIZE(3) + JSON_ARRAY_SIZE(2) + 200;
+
+// debug: just print the returning data
+//  char tst[64];
+//  while (client.available())
+//    Serial.print(char(client.read()));
+//  Serial.println();
+//  return 0;
+
   DynamicJsonDocument doc(capacity);
 
   // Parse JSON object
@@ -76,11 +90,13 @@ int get_subscriber_count()
   if (error) {
     Serial.print(F("deserializeJson() failed: "));
     Serial.println(error.c_str());
+    everythingIsFine = false;
     return -1;
   }
 
   // debug
   //serializeJsonPretty(doc, Serial);
+  everythingIsFine = true;
   return doc["items"][0]["statistics"]["subscriberCount"].as<int>();
 }
 
@@ -99,26 +115,45 @@ void InitWifi()
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
-  Serial.println(WiFi.localIP());  
+  Serial.println(WiFi.localIP());
 }
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
+  digitalWrite(LED_BUILTIN, HIGH);  // Turn the LED off
   delay(2000);
   Serial.begin(9600);
   InitWifi();
 }
 
-void loop() 
+void blink()
+{
+    digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on
+    delay(500);
+    digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED off
+
+    //double blink if something is wrong
+    if(not everythingIsFine) {
+      delay(300);
+      digitalWrite(LED_BUILTIN, LOW);   // Turn the LED on
+      delay(300);
+      digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED off      
+    }
+}
+
+void loop()
 {
   int subCount = get_subscriber_count();
   if(subCount > 0) {
     //Serial.print("getSubCount returned: ");
     //Serial.println(subCount);
-    Serial.print(String("$D")+String(subCount)+"\n");
+    Serial.print(String("$D") + String(subCount / 10) + "\n");
+    blink();
   }
-  delay(60000); // 1 minute wait
-  delay(60000); // 1 minute wait
-  delay(60000); // 1 minute wait
-  delay(60000); // 1 minute wait
-  delay(60000); // 1 minute wait
+
+  for(uint8_t c = 0; c < 5; c++) {
+    delay(60000); // 1 minute wait
+    if(not everythingIsFine)
+      blink();  
+  }
 }
